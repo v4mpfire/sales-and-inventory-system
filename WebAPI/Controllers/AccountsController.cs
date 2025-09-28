a33b1c9c58f6326dd7c67913abff5eecf223a337
Mark Gil Cordis <01PH1674.MarkGil.C@ict888.net>
2025-09-28 16:28:47 +0800

    WebAPI > User logout

--- WebAPI/Controllers/AccountsController.cs
+++ WebAPI/Controllers/AccountsController.cs
@@ -1,16 +1,25 @@
 ﻿using Application.Accounts;
+using Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers;
 
-public class AccountsController(IMediator mediator) : BaseApiController
+public class AccountsController(IMediator mediator, SignInManager<User> signInManager) : BaseApiController
 {
     [AllowAnonymous]
     [HttpPost("register")]
     public async Task<ActionResult> RegisterAsync(RegisterDTO request)
     {
         return HandleResult(await mediator.Send(new RegisterUser.Command { RegisterDTO = request }));
+    }
+
+    [HttpPost("logout")]
+    public async Task<ActionResult> Logout()
+    {
+        await signInManager.SignOutAsync();
+        return Ok();
     }
 }
